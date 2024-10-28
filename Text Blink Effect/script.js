c64556033cef26ad25918ae8bd0d43ae2f86fe2b
Anubhaba Sahani <anubhabasahani87@gmail.com>
2024-10-28 22:19:13 +0530

    Add files via upload

--- Text Blink Effect/script.js
+++ Text Blink Effect/script.js
@@ -1,1 +1,9 @@
+const blinkingText = document.getElementById('blinkingText');
 
+let isVisible = true;
+
+function toggleVisibility() {
+    isVisible = !isVisible; // Toggle the boolean value
+    blinkingText.style.display = isVisible ? 'block' : 'none'; // Show or hide the text
+}
+setInterval(toggleVisibility, 1000);
